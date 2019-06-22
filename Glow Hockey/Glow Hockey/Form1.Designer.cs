b6ab87e2df08dc83d372931ee71871e0226d9fc9
Muhammed <47753807+muhammeddeniz@users.noreply.github.com>
2019-06-22 08:23:19 +0300

    Add files via upload

---
+++ Glow Hockey/Glow Hockey/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Glow_Hockey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.myPlayer = new System.Windows.Forms.Button();
+            this.ball = new System.Windows.Forms.Button();
+            this.computerPlayer = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labellwl = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pointLabel = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.toppointLabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.healthPoint = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(450, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(443, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(870, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 325);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(870, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 388);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(676, 709);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 23);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(443, 23);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(12, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 325);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Navy;
+            this.label7.Location = new System.Drawing.Point(12, 344);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 388);
+            this.label7.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Navy;
+            this.label8.Location = new System.Drawing.Point(12, 709);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(217, 23);
+            this.label8.TabIndex = 7;
+            // 
+            // myPlayer
+            // 
+            this.myPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.myPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.myPlayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myPlayer.Location = new System.Drawing.Point(346, 697);
+            this.myPlayer.Name = "myPlayer";
+            this.myPlayer.Size = new System.Drawing.Size(213, 13);
+            this.myPlayer.TabIndex = 8;
+            this.myPlayer.UseVisualStyleBackColor = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ball.Cursor = System.Windows.Forms.Cursors.No;
+            this.ball.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ball.Location = new System.Drawing.Point(317, 313);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(41, 30);
+            this.ball.TabIndex = 9;
+            this.ball.UseVisualStyleBackColor = false;
+            // 
+            // computerPlayer
+            // 
+            this.computerPlayer.BackColor = System.Drawing.Color.Gray;
+            this.computerPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.computerPlayer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.computerPlayer.Location = new System.Drawing.Point(417, 65);
+            this.computerPlayer.Name = "computerPlayer";
+            this.computerPlayer.Size = new System.Drawing.Size(93, 50);
+            this.computerPlayer.TabIndex = 10;
+            this.computerPlayer.UseVisualStyleBackColor = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Black;
+            this.label11.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(925, 35);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(116, 30);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "LEWEL";
+            // 
+            // labellwl
+            // 
+            this.labellwl.AutoSize = true;
+            this.labellwl.BackColor = System.Drawing.Color.Black;
+            this.labellwl.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labellwl.ForeColor = System.Drawing.Color.White;
+            this.labellwl.Location = new System.Drawing.Point(1148, 35);
+            this.labellwl.Name = "labellwl";
+            this.labellwl.Size = new System.Drawing.Size(26, 30);
+            this.labellwl.TabIndex = 14;
+            this.labellwl.Text = "1";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Black;
+            this.label13.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(925, 114);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(91, 30);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "POİNT";
+            // 
+            // pointLabel
+            // 
+            this.pointLabel.AutoSize = true;
+            this.pointLabel.BackColor = System.Drawing.Color.Black;
+            this.pointLabel.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointLabel.ForeColor = System.Drawing.Color.White;
+            this.pointLabel.Location = new System.Drawing.Point(1148, 114);
+            this.pointLabel.Name = "pointLabel";
+            this.pointLabel.Size = new System.Drawing.Size(26, 30);
+            this.pointLabel.TabIndex = 16;
+            this.pointLabel.Text = "0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Black;
+            this.label15.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(925, 203);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(151, 30);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "TOP POİNT";
+            // 
+            // toppointLabel
+            // 
+            this.toppointLabel.AutoSize = true;
+            this.toppointLabel.BackColor = System.Drawing.Color.Black;
+            this.toppointLabel.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppointLabel.ForeColor = System.Drawing.Color.White;
+            this.toppointLabel.Location = new System.Drawing.Point(1148, 203);
+            this.toppointLabel.Name = "toppointLabel";
+            this.toppointLabel.Size = new System.Drawing.Size(26, 30);
+            this.toppointLabel.TabIndex = 18;
+            this.toppointLabel.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Black;
+            this.label10.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(1054, 336);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 30);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "x";
+            // 
+            // healthPoint
+            // 
+            this.healthPoint.AutoSize = true;
+            this.healthPoint.BackColor = System.Drawing.Color.Black;
+            this.healthPoint.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthPoint.ForeColor = System.Drawing.Color.White;
+            this.healthPoint.Location = new System.Drawing.Point(1076, 336);
+            this.healthPoint.Name = "healthPoint";
+            this.healthPoint.Size = new System.Drawing.Size(26, 30);
+            this.healthPoint.TabIndex = 20;
+            this.healthPoint.Text = "3";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1217, 878);
+            this.Controls.Add(this.healthPoint);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.toppointLabel);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.pointLabel);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labellwl);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.computerPlayer);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.myPlayer);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button myPlayer;
+        private System.Windows.Forms.Button ball;
+        private System.Windows.Forms.Button computerPlayer;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labellwl;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label pointLabel;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label toppointLabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label healthPoint;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Glow Hockey/Glow Hockey/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Glow_Hockey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int positionX = 5, positionY = 5, score = 0;
+        private void impactEvents()
+        {
+            computerPlayer.Location = new Point(ball.Location.X, computerPlayer.Location.Y);
+
+            if (ball.Top <= computerPlayer.Bottom)
+            {
+                positionY = positionY * -1;
+                score += 10;
+                pointLabel.Text = score.ToString();
+            }
+
+            if (ball.Bottom >= myPlayer.Top && ball.Left >= myPlayer.Left && ball.Right <= myPlayer.Right)
+            {
+                positionY *= -1;
+            }
+            else if (ball.Right >= label3.Left)
+            {
+                positionX *= -1;
+            }
+            else if (ball.Left <= label7.Right)
+            {
+                positionX *= -1;
+            }
+            else if (ball.Bottom >=label8.Top && ball.Left <=label8.Right)
+            {
+                positionY *= -1;
+            }
+            else if (ball.Bottom >= label4.Top && ball.Right >= label4.Left)
+            {
+                positionY *= -1;
+            }
+            else if (ball.Bottom >= label4.Top && ball.Right >= label4.Left)
+            {
+                positionY *= -1;
+            }
+
+
+
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            myPlayer.Left = e.X;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+       
+        }
+
+        int health = 0, highScore=0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            ball.Location = new Point(ball.Location.X + positionX, ball.Location.Y + positionY);
+            impactEvents();
+
+            if (ball.Location.Y >=680)
+            {
+                timer1.Stop();
+
+                health = int.Parse(healthPoint.Text);
+                health--;
+                if (health == 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("GAME OVER\n Top Score: " + toppointLabel.Text);
+
+                }
+                else
+                {
+                    MessageBox.Show(health + " Live You Have!");
+
+                    score = 0;
+                    pointLabel.Text = "0";
+
+                    toppointLabel.Text = pointLabel.Text;
+
+                    if (highScore < int.Parse(toppointLabel.Text)) 
+                    {
+                        highScore = int.Parse(toppointLabel.Text);
+                    }
+
+                    toppointLabel.Text = highScore.ToString();
+                    this.Refresh();
+                    ball.Location = new Point(317, 313);
+                    timer1.Start();
+                    healthPoint.Text = health.ToString();
+
+                }
+            }
+            if (score>= 100 && score <= 500)
+            {
+                timer1.Interval = 20;
+            }
+            else if (score >= 500 && score<=700)
+            {
+                timer1.Interval = 10;
+            }
+            else if (score >= 700 && score <= 1000)
+            {
+                timer1.Interval = 6;
+            }
+            else if (score >= 1000 && score <= 1500)
+            {
+                timer1.Interval = 4;
+            }
+            else if (score >= 1500)
+            {
+                timer1.Interval = 1;
+            }
+            
+        
+        }
+    }
+}
