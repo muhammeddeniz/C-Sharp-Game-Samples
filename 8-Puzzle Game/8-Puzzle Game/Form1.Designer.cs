6f71199848a795459e6ddb3cefcb9a494d235bf3
Muhammed <47753807+muhammeddeniz@users.noreply.github.com>
2019-07-16 11:33:44 +0300

    Add files via upload

---
+++ 8-Puzzle Game/8-Puzzle Game/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _8_Puzzle_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.konum1 = new System.Windows.Forms.Button();
+            this.konum2 = new System.Windows.Forms.Button();
+            this.konum3 = new System.Windows.Forms.Button();
+            this.konum7 = new System.Windows.Forms.Button();
+            this.konum6 = new System.Windows.Forms.Button();
+            this.konum9 = new System.Windows.Forms.Button();
+            this.konum4 = new System.Windows.Forms.Button();
+            this.konum5 = new System.Windows.Forms.Button();
+            this.konum8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // konum1
+            // 
+            this.konum1.BackColor = System.Drawing.Color.Aqua;
+            this.konum1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum1.Location = new System.Drawing.Point(0, 53);
+            this.konum1.Name = "konum1";
+            this.konum1.Size = new System.Drawing.Size(100, 100);
+            this.konum1.TabIndex = 5;
+            this.konum1.UseVisualStyleBackColor = false;
+            this.konum1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum2
+            // 
+            this.konum2.BackColor = System.Drawing.Color.Aqua;
+            this.konum2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum2.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum2.Location = new System.Drawing.Point(100, 53);
+            this.konum2.Name = "konum2";
+            this.konum2.Size = new System.Drawing.Size(100, 100);
+            this.konum2.TabIndex = 6;
+            this.konum2.UseVisualStyleBackColor = false;
+            this.konum2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum3
+            // 
+            this.konum3.BackColor = System.Drawing.Color.Aqua;
+            this.konum3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum3.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum3.Location = new System.Drawing.Point(200, 53);
+            this.konum3.Name = "konum3";
+            this.konum3.Size = new System.Drawing.Size(100, 100);
+            this.konum3.TabIndex = 7;
+            this.konum3.UseVisualStyleBackColor = false;
+            this.konum3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum7
+            // 
+            this.konum7.BackColor = System.Drawing.Color.Aqua;
+            this.konum7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum7.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum7.Location = new System.Drawing.Point(0, 253);
+            this.konum7.Name = "konum7";
+            this.konum7.Size = new System.Drawing.Size(100, 100);
+            this.konum7.TabIndex = 8;
+            this.konum7.UseVisualStyleBackColor = false;
+            this.konum7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum6
+            // 
+            this.konum6.BackColor = System.Drawing.Color.Aqua;
+            this.konum6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum6.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum6.Location = new System.Drawing.Point(200, 153);
+            this.konum6.Name = "konum6";
+            this.konum6.Size = new System.Drawing.Size(100, 100);
+            this.konum6.TabIndex = 9;
+            this.konum6.UseVisualStyleBackColor = false;
+            this.konum6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum9
+            // 
+            this.konum9.BackColor = System.Drawing.Color.Aqua;
+            this.konum9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum9.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum9.Location = new System.Drawing.Point(200, 253);
+            this.konum9.Name = "konum9";
+            this.konum9.Size = new System.Drawing.Size(100, 100);
+            this.konum9.TabIndex = 10;
+            this.konum9.UseVisualStyleBackColor = false;
+            this.konum9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum4
+            // 
+            this.konum4.BackColor = System.Drawing.Color.Aqua;
+            this.konum4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum4.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum4.Location = new System.Drawing.Point(0, 153);
+            this.konum4.Name = "konum4";
+            this.konum4.Size = new System.Drawing.Size(100, 100);
+            this.konum4.TabIndex = 11;
+            this.konum4.UseVisualStyleBackColor = false;
+            this.konum4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum5
+            // 
+            this.konum5.BackColor = System.Drawing.Color.Aqua;
+            this.konum5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum5.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum5.Location = new System.Drawing.Point(100, 153);
+            this.konum5.Name = "konum5";
+            this.konum5.Size = new System.Drawing.Size(100, 100);
+            this.konum5.TabIndex = 12;
+            this.konum5.UseVisualStyleBackColor = false;
+            this.konum5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // konum8
+            // 
+            this.konum8.BackColor = System.Drawing.Color.Aqua;
+            this.konum8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.konum8.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.konum8.Location = new System.Drawing.Point(100, 253);
+            this.konum8.Name = "konum8";
+            this.konum8.Size = new System.Drawing.Size(100, 100);
+            this.konum8.TabIndex = 13;
+            this.konum8.UseVisualStyleBackColor = false;
+            this.konum8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.click_all);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 52);
+            this.panel1.TabIndex = 14;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(187, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(246, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(303, 352);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.konum8);
+            this.Controls.Add(this.konum5);
+            this.Controls.Add(this.konum4);
+            this.Controls.Add(this.konum9);
+            this.Controls.Add(this.konum6);
+            this.Controls.Add(this.konum7);
+            this.Controls.Add(this.konum3);
+            this.Controls.Add(this.konum2);
+            this.Controls.Add(this.konum1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button konum1;
+        private System.Windows.Forms.Button konum2;
+        private System.Windows.Forms.Button konum3;
+        private System.Windows.Forms.Button konum7;
+        private System.Windows.Forms.Button konum6;
+        private System.Windows.Forms.Button konum9;
+        private System.Windows.Forms.Button konum4;
+        private System.Windows.Forms.Button konum5;
+        private System.Windows.Forms.Button konum8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 8-Puzzle Game/8-Puzzle Game/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_Puzzle_Game
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        Button number;
+        int temporaryX, temporaryY;
+        int score = 1000;
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void click_all(object sender, MouseEventArgs e)
+        {
+            if (counter % 2 == 0)
+            {
+                number = ((Button)sender);
+
+                counter++;
+            }
+            else
+            {
+                int locationButonX = ((Button)sender).Location.X;
+                int locationButonY = ((Button)sender).Location.Y;
+                int locationNumberX = number.Location.X;
+                int locationNumberY = number.Location.Y;
+
+                if (locationButonX-locationNumberX != 0 && locationButonY - locationNumberY != 0)
+                {
+                    MessageBox.Show("Hata!!! \nÇapraz Gidilemez.");
+                    counter++;
+                }
+                else if (((Button)sender).Text == "")
+                {
+                    temporaryX = number.Location.X;
+                    temporaryY = number.Location.Y;
+                    number.Location = ((Button)sender).Location;
+                    ((Button)sender).Location = new Point(temporaryX, temporaryY);
+
+                    counter++;
+                }
+                else
+                {
+                    MessageBox.Show("Hata!!!");
+                    counter++;
+                }
+                score -= 20;
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            konum1.Text = "5";
+            konum2.Text = "8";
+            konum3.Text = "2";
+            konum4.Text = "6";
+            konum8.Text = "1";
+            konum6.Text = "4";
+            konum7.Text = "3";
+            konum9.Text = "7";
+        }
+
+    }
+}
+
